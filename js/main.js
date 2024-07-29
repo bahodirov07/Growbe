53bd9c84f846a3528cbb9b2f1e454464cccb2e38
Abduraim Abduraimov <abduraimabduraimov@Abduraims-MacBook-Pro.local>
2024-07-29 18:21:46 +0500

    footer start

--- js/main.js
+++ js/main.js
@@ -1,8 +1,35 @@
 const navBurger = document.querySelector(".nav__burger")
 const navBurgerLine = document.querySelector(".nav__burger-line")
 const navMenu = document.querySelector(".nav__menu")
+const body = document.querySelector("body")
+const carousel = new Swiper(".carousel", {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+        slidesPerView: "1.2",
+        spaceBetween: 30,
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+})
+
+// const carousel2 = new Swiper(".carousel2", {
+//     slidesPerView: "auto",
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+// })
 
 navBurger.addEventListener("click", function () {
     navBurgerLine.classList.toggle("active")
     navMenu.classList.toggle("active")
+})
+
+body.addEventListener("click", function (event) {
+    if(event.currentTarget == event.target) navMenu.classList.add("menuOff")
+    console.log("worked");
 })
