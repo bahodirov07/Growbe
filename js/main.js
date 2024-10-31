c6ccef523d3bacc7a49060fff52aa419f716aa67
Ilhom <ilhombekbahodirov07@gmail.com>
2024-10-31 14:22:10 +0300

    Growbe websayti

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
+const body = document.querySelector("body")
 const navBurger = document.querySelector(".nav__burger")
 const navBurgerLine = document.querySelector(".nav__burger-line")
 const navMenu = document.querySelector(".nav__menu")
-const body = document.querySelector("body")
 const carousel = new Swiper(".carousel", {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-        slidesPerView: "1.2",
         spaceBetween: 30,
+        breakpoints: {
+            850: {
+                slidesPerView: "2.6"
+            },
+        },
         pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
+      loop: true
 })
-
-// const carousel2 = new Swiper(".carousel2", {
-//     slidesPerView: "auto",
-//     spaceBetween: 30,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-// })
 
 navBurger.addEventListener("click", function () {
+    body.classList.add("active")
     navBurgerLine.classList.toggle("active")
     navMenu.classList.toggle("active")
-})
-
-body.addEventListener("click", function (event) {
-    if(event.currentTarget == event.target) navMenu.classList.add("menuOff")
-    console.log("worked");
 })
